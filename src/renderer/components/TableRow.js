2ad7735e610cc732548eb40e15233599be87d12d
pinnekermv <pinnekermv@gmail.com>
2024-01-11 11:39:39 +0200

    revert caseChange

--- src/renderer/components/TableRow.js
+++ src/renderer/components/TableRow.js
@@ -59,7 +59,7 @@ export default class TableRow extends Component {
     let result = val;
 
     for (const type of values.types) {
-      for (const cat of values.typeCategories) {
+      for (const cat of values.type_categories) {
         if (cat.id === type.categoryId) {
           result = `${this.getLocaleProp(cat.i18nEventTypeCategoryName, true)}:`;
           break;
